a61536a0f8d78ff41af2fc227fe985694e8a3aa1
GorewiczM <gorewicz.mark@gmail.com>
2015-02-06 18:29:19 -0700

    Read for testing
    bubblesort java you need to manually enter the file path

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,6 +1,10 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Scanner;
+
 public class BubbleSort
 {
-	public static void sort(int[] pArray)
+	public static <T extends Comparable <T>> void sort(T[] pArray,int pLength)
 	{
 		int numCompares = pArray.length - 1;
 
@@ -9,9 +13,9 @@ public class BubbleSort
 			int last = 1;
 			for (int i = 1; i <= numCompares; i++)
 			{
-				if (pArray[i-1] > pArray[i])
+				if (pArray[i-1].compareTo(pArray[i]) == -1)
 				{
-					int temp = pArray[i-1];
+					T temp = pArray[i-1];
 					pArray[i-1] = pArray[i];
 					pArray[i] = temp;
 					last = i;
@@ -22,11 +26,56 @@ public class BubbleSort
 		
 	}
 	
-	public static void main(String args[])
+	public static void main(String args[]) throws FileNotFoundException
 	{
-		int[] arr = {45,22, 233, 55, 43, 54, 32 };
-		System.out.println(java.util.Arrays.toString(arr));
-		sort(arr);
-		System.out.println(java.util.Arrays.toString(arr));
+		int size = 10000;
+		double startTime = 0;
+		double endTime = 0;
+		double totalTime = 0;
+		Character CHARarray[] = new Character[size];
+		Integer INTarray[] = new Integer[size];
+		Double DOUBLEarray[] = new Double[size];
+		Float FLOATarray[] = new Float[size];
+		
+		//Change for file input
+		Scanner scan = new Scanner(new File("num.txt"));
+		for (int i = 0; scan.hasNextInt(); i++) 
+		{
+			int x = scan.nextInt();
+				//	(i * 3) % (size + mod);
+			CHARarray[i] = (char) (x % 26 + 65); //A-Z 
+			INTarray[i] = x;
+			DOUBLEarray[i] = x + .1;
+			FLOATarray[i] =  (float)(x + .1);
+		}
+	  	
+		startTime = System.currentTimeMillis();
+		sort(CHARarray, size);
+		endTime = System.currentTimeMillis();
+		totalTime = (endTime - startTime) / 1000;
+		System.out.println("SORT CHAR " + totalTime);
+		
+		
+		startTime = System.currentTimeMillis();
+		sort(INTarray, size);
+		endTime = System.currentTimeMillis();
+		totalTime = (endTime - startTime) / 1000;
+		System.out.println("SORT INT " + totalTime);
+		
+		
+		startTime = System.currentTimeMillis();
+		sort(FLOATarray, size);
+		endTime = System.currentTimeMillis();
+		totalTime = (endTime - startTime) / 1000;
+		System.out.println("SORT FLOAT " + totalTime);
+		
+		
+		startTime = System.currentTimeMillis();
+		sort(DOUBLEarray, size);
+		endTime = System.currentTimeMillis();
+		totalTime = (endTime - startTime) / 1000;
+		System.out.println("SORT DOUBLE " + totalTime);
+		System.out.println("End of Compare");
 	}
+	
 }
--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,7 +1,14 @@
+// Read for tests
+
 #include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+
 using namespace std;
 
-void sort(int pArray[], int pLength)
+template <typename T>
+void sort(T pArray[], int pLength)
 {
    int numCompares = pLength - 1;
 
@@ -12,7 +19,7 @@ void sort(int pArray[], int pLength)
       {
          if (pArray[i - 1] > pArray[i])
          {
-            int temp = pArray[i - 1];
+            T temp = pArray[i - 1];
             pArray[i - 1] = pArray[i];
             pArray[i] = temp;
             last = i;
@@ -21,17 +28,56 @@ void sort(int pArray[], int pLength)
       numCompares = last - 1;
    }
 }
+
 int main()
 {
-   int arr[] = { 45, 22, 233, 55, 43, 54, 32 };
-   int length = sizeof(arr)/sizeof(arr[0]);
-   for (int i = 0; i < length; i++)
-      cout << arr[i] << " , ";
-   cout << endl;
-   sort(arr,length);
-   for (int i = 0; i < length; i++)
-      cout << arr[i] << " , ";
-   cout << endl;
+   std::clock_t start;
+   std::clock_t end;
+   int size = 50000;
+   string filename;
+   std::cout << "Target file: ";
+   std::cin >> filename;
+
+   std::cout << "File size(entry's): ";
+   std::cin >> size;
+   char *CHARarray = new char[size];
+   int *INTarray = new int[size];
+   double *DOUBLEarray = new double[size];
+   float *FLOATarray = new float[size];
+
+   ifstream fin(filename.c_str());
+   int x;
+   for (int i = 0; i < size; i++) {
+      fin >> x;
+      cout << x;
+      CHARarray[i] = x % 26 + 65; //A-Z 
+      INTarray[i] = x;
+      DOUBLEarray[i] = x + .1;
+      FLOATarray[i] = x + .1;
+   }
+   fin.close();
+   
+   start = std::clock();
+   sort(CHARarray, size);
+   end = std::clock();
+   std::cout << "SORT CHARS = " << (double)(end - start) / CLOCKS_PER_SEC << '\n';
+
+   start = std::clock();
+   sort(INTarray, size);
+   end = std::clock();
+   std::cout << "SORT INTS = " << (double)(end - start) / CLOCKS_PER_SEC << '\n';
+
+   start = std::clock();
+   sort(FLOATarray, size);
+   end = std::clock();
+   std::cout << "SORT FLOATS = " << (double)(end - start) / CLOCKS_PER_SEC << '\n';
+
+   start = std::clock();
+   sort(DOUBLEarray, size);
+   end = std::clock();
+   std::cout << "SORT DOUBLES = " << (double)(end - start) / CLOCKS_PER_SEC << '\n';
+
+   cout << "End of Compare\n";
    return 0;
 }
 
